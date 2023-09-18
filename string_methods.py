58ec2f3201b9ef48a7dde89d0ac92719938a036d
Subhrajeet Ghosh <subhrajeet.ghosh@oracle.com>
2023-09-19 01:45:52 +0530

    string method

---
+++ string_methods.py
@@ -0,0 +1,9 @@
+#  Copyright (c) 2023.
+#  @Author Subhrajeet Ghosh
+
+name = "subha"
+print(len(name))  # length of the string
+print(name.find("u"))  # case sensitive
+print(name.capitalize())  # Capitalize the String
+print(name.upper())  # all os the character in the string will be uppercase
+print(name.lower())  # reverse of above
--- stringmethods.py
+++
@@ -1,6 +0,0 @@
-#  Copyright (c) 2023.
-#  @Author Subhrajeet Ghosh
-
-name = "Subha"
-print(len(name))  # length of the string
-
