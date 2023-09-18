d89eed73579dca7c621a8b655bb1b1d57cd4be89
Subhrajeet Ghosh <subhrajeet.ghosh@oracle.com>
2023-09-19 02:01:22 +0530

    type casting

---
+++ type_casting.py
@@ -0,0 +1,17 @@
+#  Copyright (c) 2023.
+#  @Author Subhrajeet Ghosh
+
+# Convert the data type of value to another data type value is called type casting
+
+i = 2  # integer
+j = 4.0  # float
+z = "3"  # string
+
+# converting i to string
+print(str(i))
+# converting j to integer
+print(int(j))
+# converting z to float
+print(float(z) * 3)  # now after converting that you can do mathmatical operations
+
+# you can do this vice versa with one another
