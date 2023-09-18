37229ff3d81e450b921c82de94cdebef5461dc27
Subhrajeet Ghosh <subhrajeet.ghosh@oracle.com>
2023-09-19 02:11:12 +0530

    hello

--- hello.py
+++ hello.py
@@ -1,5 +1,10 @@
 #  Copyright (c) 2023.
 #  @Author Subhrajeet Ghosh
 
-if __name__ == '__main__':
+if __name__ == '__main__':  # reason behind writing this to control the execution of code within a script or module
+    # It allows us to determine whether the Python script is being run as the main program or if it is being imported
+    # as a module into another script.By using this construct, you can specify which parts of your script should run
+    # when it is the main program and which parts should only run when it's imported as a module.
+    # The code block indented under if __name__ == '__main__': will only execute when the script is
+    # run as the main program.
     print("Hello World!!")  # to print something in the cursur or in CLI
---
+++ print_input.py
@@ -0,0 +1,4 @@
+#  Copyright (c) 2023.
+#  @Author Subhrajeet Ghosh
+
+input("hello write: ")
