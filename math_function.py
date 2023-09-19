e04a1173b40ca72f46fe1c770b3ec92e0615e147
Subhrajeet Ghosh <subhrajeet.ghosh@oracle.com>
2023-09-19 13:29:26 +0530

    math func

---
+++ math_function.py
@@ -0,0 +1,19 @@
+#  Copyright (c) 2023.
+#  @Author Subhrajeet Ghosh
+import math
+
+pi = 3.14
+
+a = 1
+x = 4
+y = 9
+z = 10
+
+print(round(pi))  # round the numeric variable to nearest integer
+print(math.ceil(pi))  # round the numeric variable up to the nearest integer
+print(math.floor(pi))  # round the numeric variable down to the nearest integer
+print(abs(pi))  # absolute value of the numeric variable
+print(pow(pi, 2))  # squering the numeric variable
+print(math.sqrt(9))  # square root of numeric variable
+print(max(a, x, y, z))  # finding the max number of 2 to n number of numeric variable
+print(min(a, x, y, z))  # finding the min number of 2 to n number of numeric variable
--- print_input.py
+++ print_input.py
@@ -1,4 +1,14 @@
 #  Copyright (c) 2023.
 #  @Author Subhrajeet Ghosh
 
-input("hello write: ")
+name = input("hello write: ")
+print("hello " + name)
+
+# when we accept input from user its always in String datatype
+
+age = input("write your age: ")
+age += 1  # this will give you error as now the age is String type for testing comment it out
+
+age = int(input("write your age: "))  # doing type casting for getting it in integer form
+age += 1
+print(age)
