eb7cdb882d84e6cc67348fe8440690ef370e6b1a
Subhrajeet Ghosh <subhrajeet.ghosh@oracle.com>
2023-09-20 20:51:51 +0530

    loop 1

---
+++ hackerrank/arith_operator.py
@@ -0,0 +1,9 @@
+#  Copyright (c) 2023.
+#  @Author Subhrajeet Ghosh
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a + b)
+    print(a - b)
+    print(a * b)
--- learning/loop.py
+++ learning/loop.py
@@ -1,2 +1,33 @@
 #  Copyright (c) 2023.
 #  @Author Subhrajeet Ghosh
+
+# for loop is a statement that will execute a block of code a limited amount of times only if condition met
+
+# Generate a sequence of numbers from 0 to 4
+range(5)
+
+# Generate a sequence of numbers from 2 to 6, inclusive
+range(2, 7)
+
+# Generate a sequence of numbers from 1 to 10, incrementing by 2
+range(1, 11, 2)
+
+# Generate a sequence of numbers from 10 to 1, incrementing by 1
+range(10, 0, -1)
+
+for i in range(10, 0, -1):
+    print(i)
+
+# printing character throgh for loop
+for i in "Subhaa":
+    print(i)
+
+# while loop :programming construct allows you to execute a block of code repeatedly as long as a condition is true
+name = ""
+
+while name != "test":
+    print("write your correct name")
+    name = input()
+
+# Nested loop is a loop inside another loop.
+# This means that the inner loop will be executed for each iteration of the outer loop.
