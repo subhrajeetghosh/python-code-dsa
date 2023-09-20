8ae19e37650c167f48f464e8b24ea5d6903571c8
Subhrajeet Ghosh <subhrajeet.ghosh@oracle.com>
2023-09-21 01:19:56 +0530

    list

---
+++ learning/list.py
@@ -0,0 +1,21 @@
+#  Copyright (c) 2023.
+#  @Author Subhrajeet Ghosh
+
+# list use to store multiple item in single variable
+
+car = ["farrari", "audi", "bmw", "suzuki"]
+print(car)
+car[0] = "tata"
+print(car)
+car.append("tesla")
+print(car)
+car.remove("tata")
+print(car)
+car.pop()
+print(car)
+car.insert(0, "ram")
+print(car)
+car.sort()
+print(car)
+car.clear()
+print(car)
