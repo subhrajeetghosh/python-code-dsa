e50791cb0e9201783a96d0160dec1621e5dab08d
Subhrajeet Ghosh <subhrajeet.ghosh@oracle.com>
2023-09-22 01:15:20 +0530

    index operator

---
+++ learning/index_operator.py
@@ -0,0 +1,10 @@
+#  Copyright (c) 2023.
+#  @Author Subhrajeet Ghosh
+
+my_string = "Hello, world!"
+first_character = my_string[0]
+print(first_character)
+print(my_string[0].islower())
+print(my_string[:5].upper())  # also you can write [0:5]
+print(my_string[7:])
+print(my_string[-1])  # last character in the string
