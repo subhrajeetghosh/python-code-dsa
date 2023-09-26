645ec7a44905f4abde6dfdf8261a5c718be5f635
Subhrajeet Ghosh <subhrajeet.ghosh@oracle.com>
2023-09-27 01:28:20 +0530

    class 2

--- learning/oops/nested_class.py
+++ learning/oops/nested_class.py
@@ -2,20 +2,30 @@
 #  @Author Subhrajeet Ghosh
 
 class Student:
-    def __init__(self, name):
+    def __init__(self, name, laptop_name):
         self.name = name
-        self.lap = self.Laptop()
+        self.lap = self.Laptop(laptop_name)
+
+    def show(self):
+        print(f"Student Name : {self.name}")
+        self.lap.show()
 
     class Laptop:
-        def __init__(self):
-            self.laptop_name = "Dell"
+        def __init__(self, laptop_name):
+            self.laptop_name = laptop_name
+
+        def show(self):
+            print(f"laptop Name : {self.laptop_name}")
 
 
-s1 = Student("Leso")
-s2 = Student("hisb")
+s1 = Student("Leso", "Hp")
+s2 = Student("hisb", "Dell")
+
+s1.show()
 
 l1 = s1.lap
 l2 = s2.lap
+l2.show()
 
 # you can create the inner class object inside the outer class,
 #                           or
