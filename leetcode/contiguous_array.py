5200d8bb7d41e277a9ea8f8974ca57a3ebc175a2
Subhrajeet Ghosh <subhrajeet.ghosh@oracle.com>
2024-03-16 17:39:41 +0530

    11mar24

---
+++ leetcode/contiguous_array.py
@@ -0,0 +1,17 @@
+#  Copyright (c) 2024.
+#  @Author Subhrajeet Ghosh
+from collections import defaultdict
+from typing import List
+
+
+def findMaxLength(self, nums: List[int]) -> int:
+    sum_nums = 0
+    map_dict = {0: -1}
+    result = 0
+    for i in range(len(nums)):
+        sum_nums += -1 if nums[i] == 0 else 1
+        if sum_nums in map_dict:
+            result = max(result, i - map_dict[sum_nums])
+        else:
+            map_dict[sum_nums] = i
+    return result
