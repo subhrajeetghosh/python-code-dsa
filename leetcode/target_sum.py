960a248d58c309936148925aa83729e05b631aa4
subhrgho <subhrajeet.ghosh@oracle.com>
2024-12-26 22:00:23 -0700

    26dec2024

--- leetcode/target_sum.py
+++ leetcode/target_sum.py
@@ -1,5 +1,5 @@
 class Solution:
-    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+    def findTargetSumWays_2nd(self, nums: List[int], target: int) -> int:
         return self.findWays(nums, target, -1, 0)
     
     def findWays(self, nums: List[int], target: int, index: int, sum: int) -> int:
@@ -8,3 +8,22 @@ class Solution:
         add = self.findWays(nums, target, index+1, sum + nums[index])
         substract = self.findWays(nums, target, index+1, sum - nums[index])
         return add + substract
+    
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        self.memo = {}
+        self.findWays_2(nums, target, -1, 0)
+
+    def findWays_2(self, nums: List[int], target: int, index: int, sum: int) -> int:
+            key = sum + " " + index
+            if key in self.memo:
+                 return self.memo[key]
+            index += 1
+            if index == len(nums):
+                 return 1 if target == sum else 0
+            add = self.findWays_2(nums, target, index, sum + nums[index])
+            substract = self.findWays_2(nums, target, index, sum - nums[index])
+
+            self.memo[key] = add + substract
+            return self.memo[key]
+    
+    
